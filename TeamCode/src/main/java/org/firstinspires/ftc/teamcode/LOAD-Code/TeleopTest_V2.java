8d8e33b940a22c4e5b51f57e74a198cf1f6ab9a8
NotABitcoinScam <152728412+NotABitcoinScam@users.noreply.github.com>
2025-09-02 17:46:08 -0400

    Added new Teleop that is current with PedroPathing v2.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOAD-Code/TeleopTest_V2.java
@@ -0,0 +1,48 @@
+class TeleopTest_V2 extends OpMode {
+
+    double leftTriggerMult = 0.33;
+    double rightTriggerMult = 1;
+    double defaultMult = 0.66;
+    double currentMult = defaultMult;
+
+    @Override
+    public void init() {}
+
+    /** This initializes the PoseUpdater, the mecanum drive motors, and the Panels telemetry. */
+    @Override
+    public void init_loop() {
+        telemetryM.debug("This will print your robot's position to telemetry while "
+                + "allowing robot control through a basic mecanum drive on gamepad 1.");
+        telemetryM.update(telemetry);
+        follower.update();
+        drawCurrent();
+    }
+
+    @Override
+    public void start() {
+        follower.startTeleopDrive();
+        follower.update();
+    }
+
+    /**
+     * This updates the robot's pose estimate, the simple mecanum drive, and updates the
+     * Panels telemetry with the robot's position as well as draws the robot's position.
+     */
+    @Override
+    public void loop() {
+
+        currentMult = 1;
+
+
+        follower.setTeleOpDrive(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, true);
+        follower.update();
+
+        telemetryM.debug("x:" + follower.getPose().getX());
+        telemetryM.debug("y:" + follower.getPose().getY());
+        telemetryM.debug("heading:" + follower.getPose().getHeading());
+        telemetryM.debug("total heading:" + follower.getTotalHeading());
+        telemetryM.update(telemetry);
+
+        drawCurrentAndHistory();
+    }
+}
