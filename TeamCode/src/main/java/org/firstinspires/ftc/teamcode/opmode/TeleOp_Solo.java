0bb0ff30af67ed23ecaf02dd177d6c184c1ddcec
aarushrk <aarush.kikkuru@gmail.com>
2025-10-18 14:56:39 -0700

    made the teleOp normal again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp_Solo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp_Solo.java
@@ -112,10 +112,10 @@ public class TeleOp_Solo extends CommandOpMode {
             robot.updateData();
             robot.write();
 
-            robot.follower.setTeleOpDrive(
+            follower.setTeleOpDrive(
                     -gamepad1.left_stick_y,
-                    -gamepad1.right_stick_x/4,
                     -gamepad1.left_stick_x,
+                    -gamepad1.right_stick_x/4,
                     true // Robot Centric
             );
 
