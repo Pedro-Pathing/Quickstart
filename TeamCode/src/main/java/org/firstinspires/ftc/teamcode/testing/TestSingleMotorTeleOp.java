9dd206480b788aa6479bd703e98a3fe3309e8a57
DDbrother <derekyin42009@gmail.com>
2025-11-15 20:16:07 -0500

    adds a motor testing teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestSingleMotorTeleOp.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@TeleOp(name="Motor Power", group="Test OpModes")
+public class TestSingleMotorTeleOp extends OpMode {
+
+    private List<DcMotor> motors = new ArrayList<>();
+    private DcMotor motor;
+    private int selectedIndex = 0;
+    private double power = 0.5;
+    private boolean brake = false;
+    private Gamepad currentGamepad1 = new Gamepad();
+    private Gamepad lastGamepad1 = new Gamepad();
+
+    @Override
+    public void init() {
+        // Store motors currently accessible from the HardwareMap
+        motors.addAll(
+                hardwareMap.dcMotor.entrySet()
+                        .stream()
+                        .map(Map.Entry::getValue)
+                        .collect(java.util.stream.Collectors.toList())
+        );
+    }
+
+    @Override
+    public void init_loop() {
+        lastGamepad1.copy(currentGamepad1);
+        currentGamepad1.copy(gamepad1);
+
+        // Rising Edge Detector for X --> cycle through motors
+        if (currentGamepad1.x && !lastGamepad1.x) {
+            selectedIndex++;
+            if (selectedIndex >= motors.size()) {
+                selectedIndex = 0;
+            }
+        }
+
+        // Handle telemetry
+        telemetry.addData("Selected Motor", motors.get(selectedIndex).getDeviceName());
+        telemetry.addLine("Press X to select the next motor.");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        // Store final selection
+        motor = motors.get(selectedIndex);
+    }
+
+    @Override
+    public void loop() {
+        lastGamepad1.copy(currentGamepad1);
+        currentGamepad1.copy(gamepad1);
+
+        // Rising Edge Detector for D-pad --> change power
+        if (currentGamepad1.dpad_up && !lastGamepad1.dpad_up) {
+            power +=.1;
+        } else if (currentGamepad1.dpad_down && !lastGamepad1.dpad_down) {
+            power -=.1;
+        }
+
+        // Rising Edge Detector for Right-Joystick-Y --> actually run motor
+        if (currentGamepad1.right_stick_y > .1) {
+            motor.setPower(power);
+        } else if (currentGamepad1.right_stick_y < -.1) {
+            motor.setPower(-power);
+        } else {
+            motor.setPower(0);
+        }
+
+        // Rising Edge Detector for Y --> change brake behavior
+        if (currentGamepad1.y && !lastGamepad1.y) {
+            brake = !brake;
+            motor.setZeroPowerBehavior(brake ? DcMotor.ZeroPowerBehavior.BRAKE : DcMotor.ZeroPowerBehavior.FLOAT);
+        }
+
+        // Handle telemetry
+        telemetry.addLine("Right Joystick to run");
+        telemetry.addLine("D-pad to change power (up/down +-.1)");
+        telemetry.addLine("Y to change brake behavior");
+        telemetry.addLine("");
+        telemetry.addLine("Motor")
+                .addData("Type", motor.getMotorType().getName())
+                .addData("Power", "%.3f", motor.getPower())
+                .addData("Brake?", brake);
+
+        telemetry.update();
+    }
+
+}
