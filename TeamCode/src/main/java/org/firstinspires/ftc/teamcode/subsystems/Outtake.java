d8fd793364d158d9c146ef77c9d6d370b49471a1
DDbrother <derekyin42009@gmail.com>
2025-11-19 20:02:46 -0500

    add Sloth
    lint
    
    lint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
 
 public class Outtake implements Subsystem {
 
     public static final Outtake INSTANCE = new Outtake();
+
+    private DcMotorEx flywheel;
 
     @Override
     public void initialize() {
@@ -14,4 +20,5 @@ public class Outtake implements Subsystem {
     public void periodic() {
 
     }
+
 }
