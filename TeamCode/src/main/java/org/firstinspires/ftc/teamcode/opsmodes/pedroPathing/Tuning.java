tlindhardt/Quickstart-Decode

Working code for discover fest

TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/pedroPathing/Tuning.java
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode.opsmodes.pedroPathing;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.changes;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawCurrent;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawCurrentAndHistory;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.stopRobot;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.telemetryM;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.changes;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.drawCurrent;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.drawCurrentAndHistory;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.follower;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.stopRobot;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.telemetryM;
 
 import com.bylazar.configurables.PanelsConfigurables;
 import com.bylazar.configurables.annotations.Configurable;
@@ -84,7 +84,7 @@ public class Tuning extends SelectableOpMode {
             follower = Constants.createFollower(hardwareMap);
         }
 
-        follower.setStartingPose(new Pose());
+        follower.setStartingPose(new Pose(36.0, 36.0));
 
         poseHistory = follower.getPoseHistory();
 

Pedro-Pathing/Quickstart

TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/pedroPathing/Tuning.java
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode.opsmodes.pedroPathing;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.changes;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawOnlyCurrent;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.draw;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.stopRobot;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.telemetryM;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.changes;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.drawOnlyCurrent;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.draw;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.follower;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.stopRobot;
+import static org.firstinspires.ftc.teamcode.opsmodes.pedroPathing.Tuning.telemetryM;
 
 import com.bylazar.configurables.PanelsConfigurables;
 import com.bylazar.configurables.annotations.Configurable;