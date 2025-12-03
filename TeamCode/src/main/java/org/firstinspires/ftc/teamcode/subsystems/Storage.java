8a69ae6c5da7d03263942eefc280d16dbe8f1da4
DDbrother <derekyin42009@gmail.com>
2025-12-03 16:15:19 -0500

    update xml

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.core.subsystems.SubsystemGroup;
+
+public class Storage implements Subsystem {
+    public static final Storage INSTANCE = new Storage();
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void periodic() {
+    }
+
+}
