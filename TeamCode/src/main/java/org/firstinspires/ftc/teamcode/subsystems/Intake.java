af47aa1e89cc7dc31f3359ad3ca77cdbff34a848
DDbrother <derekyin42009@gmail.com>
2025-12-11 16:11:50 -0500

    storage innit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,10 +1,20 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.RunToPosition;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.powerable.SetPower;
 
 public class Intake implements Subsystem {
 
     public static final Intake INSTANCE = new Intake();
+    public double intakePower = 0;
+
+    private MotorEx intake = new MotorEx("motorExp1")
+            .brakeMode()
+            .zeroed();
 
     @Override
     public void initialize() {
@@ -12,6 +22,21 @@ public class Intake implements Subsystem {
 
     @Override
     public void periodic() {
-
+        intake.setPower(intakePower);
     }
+
+    Command myLambdaCommand= new LambdaCommand()
+            .setStart(() -> {
+                // Runs on start
+            })
+            .setUpdate(() -> {
+                // Runs on update
+            })
+            .setStop(interrupted -> {
+                // Runs on stop
+            })
+            .setIsDone(() -> true) // Returns if the command has finished
+            .requires(/* subsystems the command implements */)
+            .setInterruptible(true)
+            .named("My Command"); // sets the name of the command; optional
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
@@ -1,11 +1,27 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import dev.nextftc.control.ControlSystem;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.core.subsystems.SubsystemGroup;
+import dev.nextftc.hardware.controllable.RunToPosition;
+import dev.nextftc.hardware.impl.MotorEx;
+
 
 public class Storage implements Subsystem {
     public static final Storage INSTANCE = new Storage();
+
+    private MotorEx spin = new MotorEx("motorExp0")
+            .brakeMode()
+            .zeroed();
+
+    private ControlSystem controlSystem = ControlSystem.builder()
+            .posPid(0.005, 0, 0)
+            .elevatorFF(0)
+            .build();
+
+    public Command toIntake1 = new RunToPosition(controlSystem, 0).requires(this);
+    public Command toIntake2 = new RunToPosition(controlSystem, 500).requires(this);
+    public Command toIntake3 = new RunToPosition(controlSystem, 1200).requires(this);
 
     @Override
     public void initialize() {
@@ -13,6 +29,7 @@ public class Storage implements Subsystem {
 
     @Override
     public void periodic() {
+        spin.setPower(controlSystem.calculate(spin.getState()));
     }
 
 }
