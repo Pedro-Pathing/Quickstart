d9fd70075e53046ee1a8af905d477c60694e5455
DDbrother <derekyin42009@gmail.com>
2025-12-17 15:57:43 -0500

    storage spin commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Storage.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.impl.MotorEx;
@@ -9,18 +11,26 @@ public class Storage implements Subsystem {
     public static final Storage INSTANCE = new Storage();
     private static boolean manualMode = true;
     private static double manualPower = 1;
-    private static MotorEx spin = new MotorEx("motor2")
-            .brakeMode();
+    private static double runPower = 0;
+    private static MotorEx spin = new MotorEx("motor2").brakeMode();
 
     private DigitalChannel limitSwitch;
     private static boolean lastState = false;
     private static int index = 0;
+    private static final State[] STATES = {
+            State.NONE,
+            State.NONE,
+            State.NONE
+    };
 
-
-    private enum PositionState {
+    // PURPLE & GREEN should take priority
+    // If the state is unknown, it should be BALL
+    // At which time we should queue for a read to determine if it's PURPLE, GREEN, or NONE
+    private enum State {
         PURPLE,
         GREEN,
-        NONE
+        NONE,
+        BALL,
     }
 
     @Override
@@ -31,19 +41,21 @@ public class Storage implements Subsystem {
 
     @Override
     public void periodic() {
+
+        // Track Indexes
+        if (wasJustPressed()) {
+            index++;
+            if (index >= STATES.length) {
+                index = 0;
+            }
+        }
+
         if (manualMode) {
             spin.setPower(manualPower);
         } else {
+            spin.setPower(runPower);
         }
 
-        // Handle state logic
-        if (wasJustPressed()) {
-            if (index == 3){
-                index = 0;
-            } else {
-                index++;
-            }
-        }
         // Write Telemetry
         ActiveOpMode.telemetry().addData("Storage Position", spin.getCurrentPosition());
         ActiveOpMode.telemetry().addData("manualmode?", manualMode);
@@ -67,5 +79,40 @@ public class Storage implements Subsystem {
         boolean triggered = current && !lastState;
         lastState = current;
         return triggered;
+    }
+
+    public static Command prioritySpin(int targetIndex) {
+        return new LambdaCommand()
+                .setStart(() -> {
+                    manualMode = false;
+                    runPower = .6;
+                })
+                .setIsDone(() -> index == targetIndex)
+
+                .setStop(interrupted -> {
+                    manualPower = 0;
+                    manualMode = true;
+                    runPower = 0;
+                })
+                .requires(Storage.INSTANCE)
+                .setInterruptible(false)
+                .named("Priority Spin → " + targetIndex);
+    }
+    public static Command lazySpin(int targetIndex) {
+        return new LambdaCommand()
+                .setStart(() -> {
+                    manualMode = false;
+                    runPower = .6;
+                })
+                .setIsDone(() -> index == targetIndex)
+
+                .setStop(interrupted -> {
+                    manualPower = 0;
+                    manualMode = true;
+                    runPower = 0;
+                })
+                .requires(Storage.INSTANCE)
+                .setInterruptible(true)
+                .named("Lazy Spin → " + targetIndex);
     }
 }
