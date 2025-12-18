7272d0682ef1cc5edce4541c5e86e6f912686181
TerenceW-0511 <terencew0511@gmail.com>
2025-12-18 16:58:26 -0500

    Panels added & MTg2 updated for pinpoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop/LimelightTest.java
@@ -1,9 +1,18 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.teleop;
 
+
+import static org.firstinspires.ftc.teamcode.pedroPathing.Drawing.drawRobot;
+import static org.firstinspires.ftc.teamcode.pedroPathing.Drawing.sendPacket;
+
+
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.kinematics.Odometry;
+import com.bylazar.field.FieldManager;
 import com.bylazar.field.PanelsField;
+import com.bylazar.field.Style;
 import com.bylazar.telemetry.PanelsTelemetry;
+import com.pedropathing.geometry.CoordinateSystem;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
@@ -11,61 +20,69 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
 
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+
 
 
 @TeleOp(name = "LimelightTest",group = "Test")
 public class LimelightTest extends OpMode {
-    private PanelsField panelsField;
 
-    private PanelsTelemetry panelsTelemetry;
+
+    private FieldManager fieldManager;
+    private Style llPos;
     private Limelight3A ll;
-    private IMU imu;
+
 
     private Odometry odo;
 
+
     @Override
     public void init() {
-        panelsField.setField(Field.PEDRO_PATHING);
-        panelsField = new PanelsField();
-        panelsTelemetry = new PanelsTelemetry(panelsField);
+        fieldManager = PanelsField.INSTANCE.getField();
+        llPos = new Style("","#3F51B5",0.75);
+        fieldManager.setOffsets(PanelsField.INSTANCE.getPresets().getPEDRO_PATHING());
         ll = hardwareMap.get(Limelight3A.class, "ll");
         ll.pipelineSwitch(1);
-        ///odo = hardwareMap.get(Odometry.class, "odo");
-        imu = hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
-        imu.initialize(new IMU.Parameters(orientation));
+        odo = hardwareMap.get(Odometry.class, "odo");
+
     }
+
 
     @Override
     public void start() {
         ll.start();
     }
 
+
     @Override
     public void loop() {
-        ///Pose2d initPose = odo.getPose();
-        ///double heading = Math.toDegrees(odo.getPose().getHeading());
-        ///ll.updateRobotOrientation(heading);
-        YawPitchRollAngles ypra = imu.getRobotYawPitchRollAngles();
-        ll.updateRobotOrientation(ypra.getYaw(AngleUnit.DEGREES));
+        double heading = Math.toDegrees(odo.getPose().getHeading());
+        ll.updateRobotOrientation(heading);
         LLResult LLResult = ll.getLatestResult();
         if (LLResult != null && LLResult.isValid()) {
             Pose3D botpose = LLResult.getBotpose_MT2();
+            Position position = botpose.getPosition();
             telemetry.addData("X", botpose.getPosition().x);
             telemetry.addData("Y", botpose.getPosition().y);
             telemetry.addData("Heading", botpose.getOrientation().getYaw(AngleUnit.DEGREES));
 
+
             double dx = botpose.getPosition().x - odo.getPose().getX();
             double dy = botpose.getPosition().y - odo.getPose().getY();
             double dist = Math.hypot(dx,dy);
+            Position pos = botpose.getPosition();
+            Pose botpose2D = new Pose(pos.x, pos.y, Math.toRadians(heading));
+            drawRobot(botpose2D,llPos);
         }
         telemetry.update();
+        fieldManager.update();
     }
 }
