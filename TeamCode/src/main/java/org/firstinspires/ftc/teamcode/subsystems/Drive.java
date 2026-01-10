57748b1aabc13c5a534282d2243cb7542b3debd2
DDbrother <derekyin42009@gmail.com>
2026-01-10 09:58:56 -0500

    t

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -93,8 +93,8 @@ public class Drive implements Subsystem {
                 telemetryM.update();
 
                 // Calculate the correct values based on Gamepad 1
-                double forward = slowMode ? -ActiveOpMode.gamepad1().left_stick_y * slowModeMultiplier: -ActiveOpMode.gamepad1().left_stick_y;
-                double strafe = slowMode ? -ActiveOpMode.gamepad1().left_stick_x * slowModeMultiplier: -ActiveOpMode.gamepad1().left_stick_x;
+                double forward = slowMode ? ActiveOpMode.gamepad1().left_stick_y * slowModeMultiplier: ActiveOpMode.gamepad1().left_stick_y;
+                double strafe = slowMode ? ActiveOpMode.gamepad1().left_stick_x * slowModeMultiplier: ActiveOpMode.gamepad1().left_stick_x;
                 double turn = slowMode ? -ActiveOpMode.gamepad1().right_stick_x * slowModeMultiplier: -ActiveOpMode.gamepad1().right_stick_x;
 
                 if (headingLock) {
