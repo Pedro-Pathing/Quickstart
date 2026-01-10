ced13870a7fc894ad28d9e7c318cbb900e33c302
DDbrother <derekyin42009@gmail.com>
2026-01-10 13:10:00 -0500

    t

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ANewJeffreyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ANewJeffreyAuto.java
@@ -27,7 +27,7 @@ import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 
 
-@Autonomous(name = "run-this-one")
+@Autonomous(name = "a-new-jeffrey-auto-run-this-one")
 public class ANewJeffreyAuto extends NextFTCOpMode {
 
     public static Pose endPose;
