2a3aedf4e2f89bbcad2822e8f23027932797432d
junkjunk123 <havitechrobo@gmail.com>
2026-09-07 18:26:34 -0700

    patch natural decel tuners to use coast not brake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/procedures/ForesightTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/procedures/ForesightTuner.java
@@ -157,7 +157,7 @@ class ForwardVelocity extends TuningOpMode<Double> {
             localizer.update();
             if (Math.abs(localizer.pose().x()) > distance) {
                 end = true;
-                drivetrain.drive(DrivePowers.zero(), true);
+                drivetrain.stop();
             } else {
                 drivetrain.drive(power, true);
                 double currentVelocity = Math.abs(localizer.twist().toVector2D().x());
@@ -166,7 +166,7 @@ class ForwardVelocity extends TuningOpMode<Double> {
             }
         }
 
-        drivetrain.drive(DrivePowers.zero(), true);
+        drivetrain.stop();
         double average = 0;
         for (double velocity : velocities) {
                 average += velocity;
@@ -212,7 +212,7 @@ class StrafeVelocity extends TuningOpMode<Double> {
             localizer.update();
             if (Math.abs(localizer.pose().y()) > distance) {
                 end = true;
-                drivetrain.drive(DrivePowers.zero(), true);
+                drivetrain.stop();
             } else {
                 drivetrain.drive(power, false);
                 double currentVelocity = Math.abs(localizer.twist().toVector2D().y());
@@ -221,7 +221,7 @@ class StrafeVelocity extends TuningOpMode<Double> {
             }
         }
 
-        drivetrain.drive(DrivePowers.zero(), true);
+        drivetrain.stop();
         double average = 0;
         for (double velocity : velocities) {
             average += velocity;
@@ -276,7 +276,7 @@ class ForwardDeceleration extends TuningOpMode<Double> {
                 previousTimeNano = System.nanoTime();
 
                 stopping = true;
-                drivetrain.drive(DrivePowers.zero(), true);
+                drivetrain.stop(false);
             }
         }
 
@@ -301,7 +301,7 @@ class ForwardDeceleration extends TuningOpMode<Double> {
             }
         }
 
-        drivetrain.drive(DrivePowers.zero(), true);
+        drivetrain.stop(false);
 
         double average = 0;
 
@@ -364,7 +364,7 @@ class StrafeDeceleration extends TuningOpMode<Double> {
                 previousTimeNano = System.nanoTime();
 
                 stopping = true;
-                drivetrain.drive(DrivePowers.zero(), true);
+                drivetrain.stop(false);
             }
         }
 
@@ -389,7 +389,7 @@ class StrafeDeceleration extends TuningOpMode<Double> {
             }
         }
 
-        drivetrain.drive(DrivePowers.zero(), true);
+        drivetrain.stop(false);
 
         double average = 0;
 
