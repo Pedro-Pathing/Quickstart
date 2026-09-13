f99356184126b1362a6fd520ef07959a3f806f55
Arush9999 <arushsai10@gmail.com>
2026-09-12 19:34:05 -0500

    Mecanum Tuner Setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode.pedro;
+import com.pedropathing.tuning.autotune.Tuner;
+import com.pedropathing.tuning.autotune.Procedure;
+import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
 
 public class Tuning {
-    // Tuners go here
+    @Tuner
+    public static Procedure mecanumTuner() {
+        return new MecanumTuner();
+    }
 }
