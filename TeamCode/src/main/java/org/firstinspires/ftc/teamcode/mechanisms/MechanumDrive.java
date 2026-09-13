aaacf0908aba878acf1f3193e325da79f6f62486
shayden <shayden@tcrobotics.org>
2026-09-13 16:05:38 -0400

    Created TeleOp1, class cleaned up and modfied MechanumDrive code, and organized files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumDrive.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MechanumDrive {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public void init(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left_motor");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right_motor");
+        backLeft = hardwareMap.get(DcMotor.class, "back_left_motor");
+        backRight = hardwareMap.get(DcMotor.class, "back_right_motor");
+
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void drive(double drive, double strafe, double turn) {
+
+        //Driving sideways usually takes more power than driving forwards
+        strafe = strafe * 1.25;
+
+        double SpeedMultiplier = 1;
+
+        double flPower = (drive + strafe + turn) * SpeedMultiplier;
+        double frPower = (drive - strafe - turn) * SpeedMultiplier;
+        double blPower = (drive - strafe + turn) * SpeedMultiplier;
+        double brPower = (drive + strafe - turn) * SpeedMultiplier;
+
+        // Normalize the values so no wheel power exceeds 100%
+        double max = Math.max(Math.abs(flPower), Math.abs(frPower));
+        max = Math.max(max, Math.abs(blPower));
+        max = Math.max(max, Math.abs(brPower));
+
+        if (max > 1.0) {
+            flPower /= max;
+            frPower /= max;
+            blPower /= max;
+            brPower /= max;
+        }
+
+        frontLeft.setPower(flPower);
+        frontRight.setPower(frPower);
+        backLeft.setPower(blPower);
+        backRight.setPower(brPower);
+    }
+}
